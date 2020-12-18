0d91e6276b9e9d374d1d68e6e985e29da632d5ec
Hong Tat, Tan <2232500+hongtat@users.noreply.github.com>
2020-12-18 23:37:13 +0800

    Bug fix

--- smartapps/hongtat/tasmota-connect.src/tasmota-connect.groovy
+++ smartapps/hongtat/tasmota-connect.src/tasmota-connect.groovy
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-String appVersion() { return "1.1" }
+String appVersion() { return "1.1.1" }
 
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
